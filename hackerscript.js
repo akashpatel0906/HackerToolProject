7eef8997e5b18ab5cdc4d14ea5e83705573ed2a8
akashpatel0906 <akashpatel0906@yahoo.com>
2018-06-17 09:07:11 -0700

    FINAL UPLOAD

---
+++ hackerscript.js
@@ -0,0 +1,210 @@
+var start=true;
+var count=0;
+var accessgranted=0;
+var accessdenied=0;
+var timercheck=0;
+var escaper=0;
+var search=0;
+var accessors = document.getElementsByClassName('access'), i;
+var randomnumber;
+var randomnumber2;
+var randomnumber3;
+var downloadTimer;
+
+<!--Below is the "code" used to pretend to be real code -->
+
+var text=";LoginBox.prototype.logout=function(){if(!this.ajax){document.getElementById(I_logoutformcharbeta).submit();return}this.elwait.innerHTML='<div class=charbetaloginmessagecharbeta><b>logging out</b><br><img src=charbeta/img/progress.gifcharbeta width=charbeta286charbeta height=charbeta20charbeta><br>please wait</div>';this.timeout=setTimeout('charbeta'+this.objName+'.modal(0)charbeta',30000);xhrAsync(CFG.ajaxlogin,bindFunction(this,this.loginoutXHR),document.getElementById(charbetaI_logoutformcharbeta).elements)};function SwitchCases(el){if(!el.hasChildNodes){return}if(typeof SwitchCases.cx==charbetaundefinedcharbeta){SwitchCases.cx=0}++SwitchCases.cx;this.objName=charbetaswitchcharbeta+SwitchCases.cx;eval(this.objName+charbeta=this;charbeta);this.elMenu=document.createElement(charbetaulcharbeta);this.labels=new Array();this.nlabels=0;this.views=new Array();this.buttons=new Array();for(var i=0;i<el.childNodes.length;++i){if(el.childNodes[i].title){this.views[this.nlabels]=el.childNodes[i];this.labels[this.nlabels]=el.childNodes[i].title;var opt=document.createElement(charbetalicharbeta);opt.innerHTML='<a href=charbetajavascript:'+this.objName+charbeta.select(charbeta+(this.nlabels+1)+')charbeta>'+el.childNodes[i].title+charbeta</a>charbeta;this.buttons[this.nlabels]=opt;this.elMenu.appendChild(opt);++this.nlabels}}var nil=document.createElement(charbetalicharbeta);nil.className=charbetahidecharbeta;var txt='<a href=charbeta/site/versions/charbeta><b class=charbetaC_ico questioncharbeta title=charbetaHelp about versionscharbeta></b></a>';nil.innerHTML=txt;this.elMenu.appendChild(nil);el.insertBefore(this.elMenu,el.firstChild);this.el=el;var autoselect=new String();autoselect+=cookieGet(CFG.cklbl);autoselect=autoselect.split(charbeta|charbeta);for(var j=0;j<autoselect.length;++j){for(var i=0;i<this.nlabels;++i){if(this.labels[i]==autoselect[j]){this.select(i+1,true);return}}}this.select(1,true)}SwitchCases.prototype.select=function(e,g){if(e){this.selected=e;this.hidden=false}else{if(this.hidden){e=this.selected;this.hidden=false}else{this.hidden=true}}for(var b=0;b<this.nlabels;++b){this.views[b].className=(b==e-1?charbetaoncharbeta:charbetaoffcharbeta);this.buttons[b].className=(b==e-1?charbetaoncharbeta:charbetaoffcharbeta)}if(!g){var c=new Array();if(e){c.push(this.labels[e-1])}var f=new String();f+=cookieGet(CFG.cklbl);f=f.split(charbeta|charbeta);for(var a=0;a<f.length;++a){if(a>10){break}var d=false;for(var b=0;b<this.nlabels;++b){if(this.labels[b]==f[a]){d=true}}if(!d){c.push(f[a])}}cookieSet(CFG.cklbl,c.join(charbeta|charbeta))}};function BoxSort(el){this.el=el;this.lu_groups=null;this.lu_alpha=null;this.persistent=true;this.alpha_on=false;if(typeof BoxSort.cx==charbetaundefinedcharbeta){BoxSort.cx=0}++BoxSort.cx;this.objName=uniqueName(el.id);eval(this.objName+charbeta=this;charbeta);if(this.persistent){if(SelectedLabel(this.objName)){this.alpha_on=true}}for(var  instancStackExchange.init(aalocalea:aena,aserverTimea:1520748365,arouteNamea:aQuestions/Showa,astackAuthUrla:ahttps://stackauth.coma,anetworkMetaHostnamea:ameta.stackexchange.coma,asitea:aanamea:aStack Overflowa,adescriptiona:aQ&A echo 55 binaryateam digit pinging a,aisNoticesTabEnableda:true,arecaptchaPublicKeya:a6LdchgIAAAAAAJwGpIzRQSOFaO0pU6s44Xt8aTwca,arecaptchaAudioLanga:aena,aenableNewTagCreationWarninga:true,ainsertSpaceAfterNameTabCompletiona:false,aida:1,achildUrla:ahttps://meta.stackoverflow.coma,aenableSocialMediaInSharePopupa:true,aprotocola:ahttpsa},ausera:aafkeya:a493bb9e10c0fc476b66ec4d41139a663a,atida:af10b1485-c317-edbe-a152-95ec4cbe1a59a,arepa:0,aisAnonymousa:true,aisAnonymousNetworkWidea:true,acanSeeNewHeaderDesigna:true},aeventsa:aapostTypea:aaquestiona:1},apostEditionSectiona:aatitlea:1,abodya:2,atagsa:3}},astorya:aaminCompleteBodyLengtha:75,alikedTagsMaxLengtha:300,adislikedTagsMaxLengtha:300},ajobPreferencesa:aamaxNumDeveloperRolesa:2,amaxNumIndustriesa:4}}, g.setCacheBreakers(aajs/prettify-full.en.jsa:a6797487791dfa,ajs/moderator.en.jsa:ab0106b7686fca,ajs/full-anon.en.jsa:a8d4ab4c4ca11a,ajs/full.en.jsa:a56773a7947e3a,ajs/wmd.en.jsa:a0429337ed546a,ajs/third-party/jquery.autocomplete.min.jsa:ad3b8fa7fdf74a,ajs/third-party/jquery.autocomplete.min.en.jsa:aa,ajs/mobile.en.jsa:ac7a7fdd3897da,ajs/help.en.jsa:aa1c2d4ed1321a,ajs/tageditor.en.jsa:a2c6c0af8eabea,ajs/tageditornew.en.jsa:a8bdeb58d6a80a,ajs/inline-tag-editing.en.jsa:ae7aa31e8eba1a,ajs/revisions.en.jsa:adb5a160e1690a,ajs/review.en.jsa:a163671c42603a,ajs/tagsuggestions.en.jsa:a78eee7c19ab2a,ajs/post-validation.en.jsa:a1d091216d9a9a,ajs/explore-qlist.en.jsa:ad36f2b82bffaa,ajs/events.en.jsa:aafb35534c199a,ajs/keyboard-shortcuts.en.jsa:aaa2e5bebe565a,ajs/external-editor.en.jsa:aa868d6711ab5a,ajs/adops.en.jsa:a22a9bd59b1e9a,ajs/external-editor.en.jsa:aa868d6711ab5a,ajs/snippet-javascript.en.jsa:a0d8955a691c8a,ajs/snippet-javascript-codemirror.en.jsa:a4b7ff04c4e72a});";
+
+<!--Below is the flashing "HAX INTIALIZED" div -->
+
+$(document).ready(function(){
+	
+	for (i=0; i<100; i++){
+		$("#starttext").fadeOut("slow");
+		 $("#starttext").fadeIn("slow");
+		}
+       $("#starttext").fadeOut("slow");
+	   
+	   
+	
+});
+
+
+<!--Below is the Popup of Access Granted div -->
+
+
+function accessGranted(){
+	var accessgranted = document.getElementById('accessgranted');
+	accessgranted.style.display = 'block';
+	
+	
+}
+
+
+
+
+
+<!--Below is the Popup of Access Denied div -->
+
+function accessDenied(){
+	var accessdenied = document.getElementById('accessdenied');
+	accessdenied.style.display = 'block';
+	
+	
+	}
+	
+
+
+<!--Below is the CHECK of Access Granted div -->
+
+function checkAccessGranted(){
+	
+	if (event.keyCode == 13) {
+        accessgranted++;
+		if (accessgranted==3){
+			accessGranted();
+			}
+    }
+	else{
+		accessgranted=0;
+		}
+	}
+<!--Below is the CHECK of Access Denied div -->
+
+function checkAccessDenied(){
+	if (event.keyCode == 32) {
+        accessdenied++;
+		if (accessdenied==3){
+			accessDenied()
+			}
+    }
+	else{
+		accessdenied=0;
+		}
+	
+	}
+	
+<!--Below is the Timer of Extermination Timer (counts down from 10 then leads to static) -->
+
+function timer(){
+	 var timeleft = 10;
+	 
+    downloadTimer = setInterval(function(){
+		
+    timeleft--;
+	document.getElementById("timer").style.display = 'block';
+    document.getElementById("timer").textContent = "Hack Attempt Found: System Termination in: " +  timeleft;
+	
+	
+		
+    if(timeleft <= 0){
+        clearInterval(downloadTimer);
+		document.getElementById("main").style.display = 'none';
+	   document.getElementById("staticgif").style.display = 'block';
+	}
+	
+    },1000);
+}	
+
+	
+<!--Below is the CHECK for the Timer command -->
+
+function checkTimer(){
+	if (event.keyCode == 17){
+        timercheck++;
+		if (timercheck==3){
+			timer();
+			}
+    }
+	else{
+		timercheck=0;
+		}
+	
+	}	
+	
+<!--Below is the CHECK for the Escape command so ALL popups will be set to "hidden" -->
+
+function checkEscape(){
+	if (event.keyCode == 27) {
+		clearInterval(downloadTimer);
+       for (var i = 0; i < accessors.length; i ++) {
+    	accessors[i].style.display = 'none';}	
+	}
+}
+<!-- Below is the searchbar commmand so the div becomes "visible" -->
+
+function searchBar(){
+	var searchbar = document.getElementById('search');
+	searchbar.style.display = 'block';
+}
+function searcher(){
+		var searchbar = document.getElementById('search');
+	searchbar.style.display = 'none';
+	$("#profilepic").fadeIn("slow");
+	$("#profile").fadeIn("slow");
+	var profilepic = document.getElementById('profilepic');
+	profilepic.style.display = 'block';
+	var profile = document.getElementById('profile');
+	profile.style.display = 'block';
+	
+	 var x = document.getElementById("searchTerm").value;
+	 document.getElementById("profilename").innerHTML = x;
+	var profilename = document.getElementById('profilename');
+	profilename.style.display = 'block';
+	}
+<!-- Below is CHECK for the searchBar command-->
+
+function checkSearchBar(){
+	if (event.keyCode == 16){
+        search++;
+		if (search==3){
+			searchBar();
+			}
+    }
+	else{
+		search=0;
+		}
+	
+	}	
+
+
+<!-- BELOW is the function that runs WHENEVER a key is typed-->
+
+	
+function myFunction(){
+	randomnumber = Math.floor(Math.random() * 300+100);
+	randomnumber2 = Math.floor(Math.random() * 100+100);
+	randomnumber3 = Math.floor(Math.random() * 100+100);
+		var starttext = document.getElementById('starttext');
+		starttext.innerHTML = "HAX INITIALIZED";
+	
+	checkAccessGranted();
+	checkAccessDenied();
+     checkEscape();
+	 checkTimer();
+	 checkSearchBar();
+		
+		
+	var div = document.getElementById('terminal');
+
+	div.innerHTML += text.substr(count,[5]);
+	count+=5;
+<!-- BELOW is how the "code" string is made to look like real code so it randomly creates indents and loops based on substrings-->
+
+	
+	if (count%randomnumber==0){
+		div.innerHTML += "<br />" + "<br />" + text.substr(count,[5]) + "{" + "<br />"+ "&nbsp;" + "&nbsp;" + "&nbsp;" ;
+		}
+	if (count%randomnumber2==0){
+		div.innerHTML += "<br />" + "&nbsp;" + "&nbsp;" + "&nbsp;" ;
+		}
+
+		
+	
+	if (count>=text.length){
+		count=0;
+		
+	}
+
+	
+}
